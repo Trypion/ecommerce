ded04b284e92ffc74277b18adc8a3bc1535ecc9d
Israel Schmitt <israel.schmitt.j@gmail.com>
2025-09-18 19:30:30 -0300

    user-service init

---
+++ user-service/internal/config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DatabaseURL string
+	Port        string
+	DBHost      string
+	DBPort      int
+	DBUser      string
+	DBPassword  string
+	DBName      string
+}
+
+func Load() *Config {
+
+	if err := godotenv.Load(); err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	dbPort, _ := strconv.Atoi(getEnv("DB_PORT", "5432"))
+
+	return &Config{
+		DatabaseURL: getEnv("DATABASE_URL", ""),
+		Port:        getEnv("PORT", "50051"),
+		DBHost:      getEnv("DB_HOST", "localhost"),
+		DBPort:      dbPort,
+		DBUser:      getEnv("DB_USER", "postgres"),
+		DBPassword:  getEnv("DB_PASSWORD", "postgres"),
+		DBName:      getEnv("DB_NAME", "orders_service"),
+	}
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+
+	return defaultValue
+}
