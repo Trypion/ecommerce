7bdfe3a42896877019c22cff3b821b49e917a567
Israel Schmitt <israel.schmitt.j@gmail.com>
2025-12-08 20:04:34 -0300

    docker and env files

--- deployments/init-db.sql
+++ deployments/init-db.sql
@@ -1,7 +1,9 @@
 -- Create databases for each service
 CREATE DATABASE order_service;
 CREATE DATABASE payment_service;
+CREATE DATABASE user_service;
 
 -- Grant all privileges to the ecommerce user
 GRANT ALL PRIVILEGES ON DATABASE order_service TO ecommerce;
 GRANT ALL PRIVILEGES ON DATABASE payment_service TO ecommerce;
+GRANT ALL PRIVILEGES ON DATABASE user_service TO ecommerce;
